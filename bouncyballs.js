89d9fa22e9e5ac1ec2e8321d9bfd932fc07774be
igerstein <isaac.gerstein1@gmail.com>
2016-04-21 17:45:03 -0400

    Added randomness and collision (glitchy when there are many balls)

--- bouncyballs.js
+++ bouncyballs.js
@@ -8,23 +8,16 @@ var Ball = function(x, y, radius, dx, dy, color){
   this.radius = radius || 20;
   this.dx = dx || 10;
   this.dy = dy || 10;
-  this.color = color || '#FFFFFF';
+  this.color = color || 'black';
   this.draw();
 };
 
 Ball.prototype.draw = function(){
   ctx.beginPath();
+  ctx.lineWidth = 0;
   ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+  ctx.fillStyle = this.color;
   ctx.fill();
-  ctx.stroke();
-};
-
-Ball.prototype.setX = function(x){
-  this.x = x;
-};
-
-Ball.prototype.setY = function(y){
-  this.y = y;
 };
 
 Ball.prototype.reflect = function(canvas, balls){
@@ -32,20 +25,19 @@ Ball.prototype.reflect = function(canvas, balls){
   if(this.y + this.dy + this.radius >= canvas.height || this.y + this.dy - this.radius < 0) this.dy *= -1;
   for(var ballkey in balls){
     var ball = balls[ballkey];
-    var sumOfRad = this.radius + ball.radius;
-    var diffOfRad = Math.abs(this.radius - ball.radius);
-    var distance = Math.sqrt( ( this.x + ball.x ) * (this.x + ball.x) + (this.y + ball.y) * (this.y + ball.y));
-    if(sumOfRad >= distance && diffOfRad <= distance){
-      this.dy = (Math.random() + 1) * 10;
-      this.dx = (Math.random() + 1) * 10;
-      ball.dy = (Math.random() + 1) * 10;
-      ball.dx = (Math.random() + 1) * 10;
+    if (this != ball){
+      diffRadiiSquared = Math.pow(this.radius - ball.radius, 2);
+      distCentersSquared = Math.pow(this.x - ball.x, 2) + Math.pow(this.y - ball.y, 2);
+      sumRadiiSquared = Math.pow(this.radius + ball.radius, 2);
+      if (diffRadiiSquared <= distCentersSquared && distCentersSquared <= sumRadiiSquared){
+	this.dx *= -1;
+	this.dy *= -1;
+      }
     }
   }
 };
 
 Ball.prototype.move = function(){
-  this.reflect(canvas, ballpen);
   this.x += this.dx;
   this.y += this.dy;
   this.draw();
@@ -56,6 +48,7 @@ var startAnimate = function(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (var ballkey in ballpen){
       var ball = ballpen[ballkey];
+      ball.reflect(canvas, ballpen);
       ball.move();
     }
     id = window.requestAnimationFrame(animate);
@@ -68,8 +61,13 @@ var startAnimate = function(){
 
 var addBallButton = document.getElementById('addBallButton');
 addBallButton.addEventListener('click', function(){
-  var ball = new Ball();
-  console.log('hi');
+  var x = Math.floor(Math.random() * (canvas.width - 40)) + 20;
+  var y = Math.floor(Math.random() * (canvas.height - 40)) + 20;
+  var radius = Math.floor(Math.random() * 25) + 5;
+  var dx = Math.floor(Math.random() * 10) + 1;
+  var dy = Math.floor(Math.random() * 10) + 1;
+  var color = randomColor();
+  var ball = new Ball(x, y, radius, dx, dy, color);
   ballpen.push(ball);
 });
 
@@ -77,3 +75,22 @@ var startButton = document.getElementById('startButton');
 startButton.addEventListener('click', function(){
   startAnimate();
 });
+
+var randomColor = function(){
+  var r = Math.random();
+  if (r < 1/7){
+    return "black";
+  }else if (r < 2/7){
+    return "red";
+  }else if (r < 3/7){
+    return "blue";
+  }else if (r < 4/7){
+    return "yellow";
+  }else if (r < 5/7){
+    return "green";
+  }else if (r < 6/7){
+    return "orange";
+  }else{
+    return "purple";
+  }
+};
